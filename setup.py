c7d55ca79aaf2d5871c0d79b333db03fe92c37d8
Pascal-Benink <112872169+pascal-benink@users.noreply.github.com>
2024-09-13 15:08:03 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='file-management-package',
+    version='0.6.1',
+    packages=['file_management_package'],
+    install_requires=['setuptools'],
+    author='Pascal Benink',
+    author_email='2sam2samb+PythonFile@gmail.com',
+    description='A package for file management'
+)
